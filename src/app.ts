7c32b38bdaff35993f22842e6124c175373141b5
Augusto E Ringuelet <eze87.lp@hotmail.com>
2025-02-14 11:12:34 +0100

    added prisma, added prettier, start splitting routes

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import { createRoutes } from './routes';
-require('dotenv').config()
+// eslint-disable-next-line @typescript-eslint/no-require-imports
+require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 3001;
@@ -8,5 +9,5 @@ const port = process.env.PORT || 3001;
 createRoutes(app);
 
 app.listen(port, () => {
-  return console.log(`Express is listening at http://localhost:${port}`);
+    return console.log(`Express is listening at http://localhost:${port}`);
 });
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,7 +1,8 @@
 import { Express, Router } from 'express';
-import { userRoutes } from './users';
+import { userRoutes } from './user';
 
 export const createRoutes = (app: Express) => {
-  const router = Router();
-  app.use('/user', userRoutes(router));
+    const router = Router();
+
+    app.use('/user', userRoutes(router));
 };
